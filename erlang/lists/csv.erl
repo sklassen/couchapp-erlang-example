585682ec3a8822aaf8e50ebeed8a0e2cef6fe3f5
sklassen <none>
2020-03-02 20:28:38 +0800

    Erlang list matches javascript

--- erlang/lists/csv.erl
+++ erlang/lists/csv.erl
@@ -2,8 +2,8 @@ fun(Head, {Req}) ->
   Fun = fun({Row}, Acc) ->
     Id = couch_util:get_value(<<"id">>, Row),
     [A|[B|[C]]]= couch_util:get_value(<<"key">>, Row),
-    Value= couch_util:get_value(<<"value">>, Row),
-	Send([A,<<"/">>,B,<<",">>,C,<<",">>,Value,<<"\n">>]),
+    Value=couch_util:get_value(<<"value">>, Row),
+	Send([A,<<"/">>,B,<<",">>,C,<<",">>,integer_to_binary(Value),<<"\n">>]),
     {ok,Id}
   end,
   FoldRows(Fun, nil),
